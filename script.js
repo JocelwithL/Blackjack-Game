1a750fb90c71e05f91a9338e92ae3ef191074aa5
JocelwithL <rellesjocel@agmail.com>
2023-02-18 01:31:25 +0800

    Update BlackJack game by generating random number and restricting players to get another card once dead

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-let firstCard = 11;
-let secondCard = 7;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let sumEl = document.getElementById("sum-el");
 let messageEl = document.getElementById("message-el");
 let cardEl = document.querySelector("#card-el");
+// let playerName = "Jocel";
+// let playerChips = 145;
+// let playerEl = document.getElementById("player-el");
 
+// playerEl.innerHTML = playerName + ": $" + playerChips;
 
+function getRandomCard() {
+     let randomNum = Math.floor(Math.random() * 13) + 1;
+     if(randomNum > 10) {
+        return 10;
+    }
+
+    else if (randomNum === 1) {
+        return 11;
+    }
+
+    else {
+        return randomNum
+    }
+}
 
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame()
+}
+
+function renderGame() {
 if (sum < 21) {
     message = "Want another card?";
 }
@@ -23,10 +49,25 @@ else {
     message = "You're out!";
     isAlive = false;
 }
+
 sumEl.innerHTML = "Sum: " + sum;
-cardEl.innerHTML = "Cards: " + firstCard + " - " + secondCard;
-messageEl.innerHTML = message;
-console.log(message)
+cardEl.innerHTML = "Cards: "
+for(let i=0; i < cards.length; i++) {
+    cardEl.innerHTML += cards[i] + " ";
 }
+messageEl.innerHTML = message;
+}
+
+function getNewCard() {
+    if (hasBlackJack === false && isAlive === true) {
+        let anotherCard = getRandomCard();
+        sum = sum + anotherCard;
+        cards.push(anotherCard)
+        renderGame()
+    }
+    
+}
+
+// 3:21:00
 
 
